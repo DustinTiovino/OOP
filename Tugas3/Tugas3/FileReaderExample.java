0fb63cd830c893f1ee195b92362c153ec46f91a9
Dustin <dustin.tiovino@itbss.ac.id>
2023-11-23 13:18:28 +0700

    test

--- Tugas3/Tugas3/FileReaderExample.java
+++ Tugas3/Tugas3/FileReaderExample.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 
 public class FileReaderExample{
     public static void main(String[] args) {
-        String fileName = "FileReaderExample.txt"; // Ganti dengan nama file yang sesuai
+        String fileName = "File.txt"; // Ganti dengan nama file yang sesuai
 
         try {
             String isiFile = bacaFile(fileName);
